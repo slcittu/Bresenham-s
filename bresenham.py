338399921b19809710b9b236ede02c427456d0ee
Cittu S L <slcittu@gmail.com>
2024-06-20 15:11:00 +0530

    Defined function for Bresenham's Circle drawing algorithm

--- bresenham.py
+++ bresenham.py
@@ -9,3 +9,21 @@ def draw_circle(xc, yc, x, y, points):
     points.append((xc - y, yc + x))
     points.append((xc + y, yc - x))
     points.append((xc - y, yc - x))
+
+def bresenham_circle(xc, yc, radius):
+    x = 0
+    y = radius
+    p = 3 - 2 * radius
+    points = []
+    draw_circle(xc, yc, x, y, points)
+    
+    while y >= x:
+        x += 1
+        if p > 0:
+            y -= 1
+            p = p + 4 * (x - y) + 10
+        else:
+            p = p + 4 * x + 6
+        draw_circle(xc, yc, x, y, points)
+    
+    return points
