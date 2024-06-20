fabd59644e896cb7dbf748f0759d624839b154c7
Cittu S L <slcittu@gmail.com>
2024-06-20 14:59:13 +0530

    Defined function for Ploting 8 points to draw circle

--- bresenham.py
+++ bresenham.py
@@ -1,1 +1,11 @@
 import matplotlib.pyplot as plt
+
+def draw_circle(xc, yc, x, y, points):
+    points.append((xc + x, yc + y))
+    points.append((xc - x, yc + y))
+    points.append((xc + x, yc - y))
+    points.append((xc - x, yc - y))
+    points.append((xc + y, yc + x))
+    points.append((xc - y, yc + x))
+    points.append((xc + y, yc - x))
+    points.append((xc - y, yc - x))
